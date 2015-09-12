19ad25f0152d12d8b03e04e2531ecbab13528683
Oleksii Fedorov <waterlink000@gmail.com>
2015-09-13 01:42:37 +0200

    remove obvious type annotations

--- parser.hs
+++ parser.hs
@@ -12,7 +12,6 @@ import Text.ParserCombinators.Parsec ((<|>))
 import qualified Text.ParserCombinators.Parsec as P
 import qualified Text.Parsec.String as PS
 
-main :: IO()
 main = do contents <- getContents
           let output = case P.parse final "$STDIN" contents of
                             Left err -> "No match: " ++ show err
@@ -23,30 +22,22 @@ main = do contents <- getContents
 
 -- Characters
 
-newline :: P.Parser Char
 newline = P.newline
 
-unicodeChar :: P.Parser Char
 unicodeChar = P.anyChar
 
-unicodeLetter :: P.Parser Char
 unicodeLetter = P.letter
 
-unicodeDigit :: P.Parser Char
 unicodeDigit = P.digit
 
 -- Letters and digits
 
-letter :: P.Parser Char
 letter = unicodeLetter <|> P.char '_'
 
-decimalDigit :: P.Parser Char
 decimalDigit = P.oneOf "0123456789"
 
-octalDigit :: P.Parser Char
 octalDigit = P.oneOf "01234567"
 
-hexDigit :: P.Parser Char
 hexDigit = P.oneOf "0123456789ABCDEFabcdef"
 
 -- # Lexical elements
@@ -80,19 +71,16 @@ instance Representable LexicalElement where
 
 -- Comments
 
-lineComment :: P.Parser LexicalElement
 lineComment = do string "//"
                  text <- P.manyTill unicodeChar (P.try newline)
                  return $ LineComment text
 
-generalComment :: P.Parser LexicalElement
 generalComment = do string "/*"
                     text <- P.manyTill unicodeChar (string "*/")
                     return $ GeneralComment text
 
 -- Tokens
 
-identifier :: P.Parser LexicalElement
 identifier = do first <- letter
                 rest <- P.many $ letter <|> unicodeDigit
                 return $ Identifier (first:rest)
@@ -104,10 +92,8 @@ keywords = [
             "const",        "fallthrough",  "if",           "range",        "type",
             "continue",     "for",          "import",       "return",       "var"]
 
-parseKeyword :: P.Parser String
 parseKeyword = P.choice $ map word keywords
 
-keyword :: P.Parser LexicalElement
 keyword = do name <- parseKeyword
              return $ Keyword name
 
@@ -121,27 +107,21 @@ operators = [
 countOperatorsWithThatPrefix x = length $ filter (isPrefixOf x) operators
 parseOrderedOperators = sortBy (comparing countOperatorsWithThatPrefix) operators
 
-parseOperator :: P.Parser String
 parseOperator = P.choice $ map string parseOrderedOperators
 
-operator :: P.Parser LexicalElement
 operator = do name <- parseOperator
               return $ Operator name
 
-integerLiteral :: P.Parser LexicalElement
 integerLiteral = decimalLiteral <|> hexLiteral <|> octalLiteral
 
-decimalLiteral :: P.Parser LexicalElement
 decimalLiteral = P.try $ do first <- P.oneOf "123456789"
                             rest <- P.many decimalDigit
                             return $ IntegerLiteral (Decimal $ first:rest)
 
-octalLiteral :: P.Parser LexicalElement
 octalLiteral = P.try $ do first <- P.char '0'
                           rest <- P.many octalDigit
                           return $ IntegerLiteral (Octal $ first:rest)
 
-hexLiteral :: P.Parser LexicalElement
 hexLiteral = P.try $ do first <- P.char '0'
                         middle <- P.oneOf "xX"
                         rest <- P.many hexDigit
