d1cbe93273ab36913a36f7adafe797ff3278e02f
Oleksii Fedorov <waterlink000@gmail.com>
2015-09-26 00:33:51 +0200

    More WIP on codegen

--- Codegen.hs
+++ Codegen.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
 module Codegen where
 
 import LLVM.General.AST
@@ -8,5 +11,97 @@ import qualified LLVM.General.AST.Constant as C
 import qualified LLVM.General.AST.Attribute as A
 import qualified LLVM.General.AST.CallingConvention as CC
 
+import Control.Monad.State
+import Control.Applicative
+
+import Data.String
+import Data.Word
+import qualified Data.Map as Map
+
 emptyModule :: String -> AST.Module
 emptyModule label = defaultModule { moduleName = label }
+
+newtype LLVM a = LLVM { unLLVM :: State AST.Module a }
+  deriving (Functor, Applicative, Monad, MonadState AST.Module)
+
+runLLVM :: AST.Module -> LLVM a -> AST.Module
+runLLVM = flip (execState . unLLVM)
+
+addDefn :: Definition -> LLVM ()
+addDefn d = do
+  defs <- gets moduleDefinitions
+  modify $ \s -> s { moduleDefinitions = defs ++ [d] }
+
+define :: Type -> String -> [(Type, Name)] -> [BasicBlock] -> LLVM ()
+define retty label argtys body = addDefn $
+  GlobalDefinition $ functionDefaults {
+    name        = Name label
+  , parameters  = ([Parameter ty nm [] | (ty, nm) <- argtys], False)
+  , returnType  = retty
+  , basicBlocks = body
+  }
+
+-- # Names
+
+type Names = Map.Map String Int
+
+uniqueName :: String -> Names -> (String, Names)
+uniqueName name names =
+  case Map.lookup name names of
+    Nothing -> (name, Map.insert name 1 names)
+    Just ix -> (name ++ show ix, Map.insert name (ix+1) names) 
+
+instance IsString Name where
+  fromString = Name . fromString
+
+-- # Codegen & block state
+
+type BlocksState = Map.Map Name BlockState
+type SymbolTable = [(String, Operand)]
+
+data CodegenState
+  = CodegenState {
+    currentBlock :: Name                   -- current active block in codegen process
+  , blocks       :: BlocksState            -- blocks for function/method
+  , symtab       :: SymbolTable            -- function/method scope symbol table
+  , blockCount   :: Int                    -- count of basic blocks
+  , count        :: Word                   -- count of unnamed instructions
+  , names        :: Names                  -- unique name supply
+  } deriving Show
+
+data BlockState
+  = BlockState {
+    idx    :: Int                          -- block index
+  , stack  :: [Named Instruction]          -- instruction stack
+  , term   :: Maybe (Named Terminator)     -- block terminator
+  } deriving Show
+  
+newtype Codegen a = Codegen { runCodegen :: State CodegenState a }
+  deriving (Functor, Applicative, Monad, MonadState CodegenState)
+
+-- # Block functions
+
+entry :: Codegen Name
+entry = gets currentBlock
+
+emptyBlock :: Int -> BlockState
+emptyBlock idx = BlockState { idx = idx
+                            , stack = []
+                            , term = Nothing
+                            }
+
+addBlock :: String -> Codegen Name
+addBlock name = do
+  bls <- gets blocks
+  ix  <- gets blockCount
+  nms <- gets names
+
+  let new = emptyBlock ix
+      (uniqName, supply) = uniqueName name nms
+
+  modify $ \s -> s { blocks = Map.insert (Name uniqName) new bls
+                   , blockCount = ix + 1
+                   , names = supply
+                   }
+
+  return $ Name uniqName
--- Emit.hs
+++ Emit.hs
@@ -1,20 +1,50 @@
 module Emit where
 
-import qualified LLVM.General.Module
-import qualified LLVM.General.Context
+import Control.Monad.Except
+import Control.Applicative
+
+import qualified LLVM.General.Module as Module
+import qualified LLVM.General.Context as Context
 
 import qualified LLVM.General.AST as AST
 import qualified LLVM.General.AST.Constant as C
+import qualified LLVM.General.AST.Type as T
 
 import qualified Data.Map as Map
 
 import Codegen
 import qualified Syntax as S
 
+liftError :: ExceptT String IO a -> IO a
+liftError = runExceptT >=> either fail return
+
+codegenTop :: S.TopLevelDecl -> LLVM ()
+codegenTop (S.FunctionDecl name (S.Signature args result) (Just body)) = do
+  define resultType name funcArgs blocks
+  where
+    resultType = resultTypeOf result
+    funcArgs = funcArgsOf args
+    blocks = blocksFor body
+    funcArgsOf = concat $ map funcArgOf
+
+    -- FIXME: supports currently only void and int
+    resultTypeOf Nothing = T.void
+    resultTypeOf (Just (S.FunctionSingleResult _)) = T.i64
+
+    -- FIXME:
+    -- - ignores type
+    -- - does not implement ellipsis
+    funcArgOf (S.ParameterDecl names _ Nothing) = map $ \name -> (T.i64, name)
+
+    -- FIXME: ignores provided list of statements entirely
+    blocksFor _ = createBlocks $ execCodegen $ do
+      entry <- addBlock entryBlockName
+      setBlock entry
+
 codegen :: AST.Module -> [S.TopLevelDecl] -> IO AST.Module
-codegen mod decls = withContext $ \context ->
-  liftError $ withModuleFromAST context newast $ \m -> do
-    llstr <- moduleLLVMAssembly m
+codegen mod decls = Context.withContext $ \context ->
+  liftError $ Module.withModuleFromAST context newast $ \m -> do
+    llstr <- Module.moduleLLVMAssembly m
     putStrLn llstr
     return newast
   where
