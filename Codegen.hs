09a09fe34a65cca0244206581f4bc38544b7838d
Oleksii Fedorov <waterlink000@gmail.com>
2015-09-26 02:56:16 +0200

    WIP: implement simplest possible + operation

--- Codegen.hs
+++ Codegen.hs
@@ -8,6 +8,7 @@ import LLVM.General.AST.Global
 import qualified LLVM.General.AST as AST
 
 import qualified LLVM.General.AST.Constant as C
+import qualified LLVM.General.AST.Type as T
 import qualified LLVM.General.AST.Attribute as A
 import qualified LLVM.General.AST.CallingConvention as CC
 
@@ -16,6 +17,8 @@ import Control.Applicative
 
 import Data.String
 import Data.Word
+import Data.List
+import Data.Function
 import qualified Data.Map as Map
 
 emptyModule :: String -> AST.Module
@@ -79,6 +82,18 @@ data BlockState
 newtype Codegen a = Codegen { runCodegen :: State CodegenState a }
   deriving (Functor, Applicative, Monad, MonadState CodegenState)
 
+sortBlocks :: [(Name, BlockState)] -> [(Name, BlockState)]
+sortBlocks = sortBy (compare `on` (idx . snd))
+
+createBlocks :: CodegenState -> [BasicBlock]
+createBlocks m = map makeBlock $ sortBlocks $ Map.toList (blocks m)
+
+makeBlock :: (Name, BlockState) -> BasicBlock
+makeBlock (l, (BlockState _ s t)) = BasicBlock l s (maketerm t)
+  where
+    maketerm (Just x) = x
+    maketerm Nothing = error $ "Block has no terminator: " ++ (show l)
+
 -- # Block functions
 
 entry :: Codegen Name
@@ -105,3 +120,98 @@ addBlock name = do
                    }
 
   return $ Name uniqName
+
+setBlock :: Name -> Codegen Name
+setBlock name = do
+  modify $ \s -> s { currentBlock = name }
+  return name
+
+getBlock :: Codegen Name
+getBlock = gets currentBlock
+
+modifyBlock :: BlockState -> Codegen ()
+modifyBlock new = do
+  active <- gets currentBlock
+  modify $ \s -> s { blocks = Map.insert active new (blocks s) }
+
+current :: Codegen BlockState
+current = do
+  c <- gets currentBlock
+  blks <- gets blocks
+  case Map.lookup c blks of
+    Just x -> return x
+    Nothing -> error $ "No such block: " ++ show c
+
+-- # LLVM instructions
+
+entryBlockName :: String
+entryBlockName = "entry"
+
+emptyCodegen :: CodegenState
+emptyCodegen = CodegenState (Name entryBlockName) Map.empty [] 1 0 Map.empty
+
+execCodegen :: Codegen a -> CodegenState
+execCodegen m = execState (runCodegen m) emptyCodegen
+
+fresh :: Codegen Word
+fresh = do
+  i <- gets count
+  let next = 1 + i
+  modify $ \s -> s { count = next }
+  return $ next
+
+local :: Name -> Operand
+local = LocalReference T.i64
+
+-- # Arithmetic
+
+iadd :: Operand -> Operand -> Codegen Operand
+iadd a b = instr $ Add False False a b []
+
+cons :: C.Constant -> Operand
+cons = ConstantOperand
+
+-- # Vars
+
+assign :: String -> Operand -> Codegen ()
+assign var operand = do
+  currentSymtab <- gets symtab
+  modify $ \s -> s { symtab = [(var, operand)] ++ currentSymtab }
+
+getvar :: String -> Codegen Operand
+getvar var = do
+  syms <- gets symtab
+  case lookup var syms of
+    Just x -> return x
+    Nothing -> error $ "Local variable not in scope: " ++ show var
+
+instr :: Instruction -> Codegen Operand
+instr ins = do
+  n <- fresh
+  let ref = (UnName n)
+  blk <- current
+  let i = stack blk
+  modifyBlock (blk { stack = i ++ [ref := ins] } )
+  return $ local ref
+
+terminator :: Named Terminator -> Codegen (Named Terminator)
+terminator trm = do
+  blk <- current
+  modifyBlock (blk { term = Just trm })
+  return trm
+
+-- # Effects
+
+alloca :: Type -> Codegen Operand
+alloca ty = instr $ Alloca ty Nothing 0 []
+
+store :: Operand -> Operand -> Codegen Operand
+store ptr val = instr $ Store False ptr val Nothing 0 []
+
+-- # Control flow
+
+ret :: Operand -> Codegen (Named Terminator)
+ret val = terminator $ Do $ Ret (Just val) []
+
+voidRet :: Codegen (Named Terminator)
+voidRet = terminator $ Do $ Ret Nothing []
--- Emit.hs
+++ Emit.hs
@@ -25,7 +25,8 @@ codegenTop (S.FunctionDecl name (S.Signature args result) (Just body)) = do
     resultType = resultTypeOf result
     funcArgs = funcArgsOf args
     blocks = blocksFor body
-    funcArgsOf = concat $ map funcArgOf
+
+    funcArgsOf args = concat $ map funcArgOf args
 
     -- FIXME: supports currently only void and int
     resultTypeOf Nothing = T.void
@@ -34,12 +35,20 @@ codegenTop (S.FunctionDecl name (S.Signature args result) (Just body)) = do
     -- FIXME:
     -- - ignores type
     -- - does not implement ellipsis
-    funcArgOf (S.ParameterDecl names _ Nothing) = map $ \name -> (T.i64, name)
+    funcArgOf (S.ParameterDecl names _ Nothing) = map (\name -> (T.i64, (AST.Name name))) names
 
     -- FIXME: ignores provided list of statements entirely
-    blocksFor _ = createBlocks $ execCodegen $ do
+    blocksFor body = createBlocks $ execCodegen $ do
       entry <- addBlock entryBlockName
       setBlock entry
+      forM funcArgs $ \(ty, (AST.Name name)) -> do
+        var <- alloca ty
+        store var $ local (AST.Name name)
+        assign name var
+      forM body $ \statement -> do
+        result <- cgen statement
+        assign "_" result
+      voidRet
 
 codegen :: AST.Module -> [S.TopLevelDecl] -> IO AST.Module
 codegen mod decls = Context.withContext $ \context ->
@@ -50,3 +59,19 @@ codegen mod decls = Context.withContext $ \context ->
   where
     modn    = mapM codegenTop decls
     newast  = runLLVM mod modn
+
+cgen :: S.Statement -> Codegen AST.Operand
+cgen (S.SimpleStmt (S.ExpressionStmt expr)) = cgenexpr expr
+
+cgen statement = error $ "Unable to understand statement: " ++ show statement
+
+cgenexpr :: S.Expression -> Codegen AST.Operand
+cgenexpr (S.Binary (S.AddBinOp S.Plus) a b) = do
+  ca <- cgenexpr a
+  cb <- cgenexpr b
+  iadd ca cb
+
+cgenexpr (S.Unary (S.PrimaryExpr (S.Operand (S.Literal (S.IntLiteral value))))) = return $ cons $ C.Int 64 value
+cgenexpr (S.Unary (S.PrimaryExpr (S.Operand (S.OperandName (S.OperandIdentifier name))))) = getvar name
+
+cgenexpr expr = error $ "Unable to understand expression: " ++ show expr
